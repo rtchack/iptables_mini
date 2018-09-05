b683b421d324e99d6e944a1ec2af7c050eef133e
Xing <thehxpax@gmail.com>
2018-09-06 03:49:11 +0800

    init

---
+++ iptables-multi.h
@@ -0,0 +1,12 @@
+#ifndef _IPTABLES_MULTI_H
+#define _IPTABLES_MULTI_H 1
+
+/**
+ * @return 0 on success, failed otherwise
+ */
+extern int iptables_main(int, char **);
+
+//extern int iptables_save_main(int, char **);
+//extern int iptables_restore_main(int, char **);
+
+#endif /* _IPTABLES_MULTI_H */
